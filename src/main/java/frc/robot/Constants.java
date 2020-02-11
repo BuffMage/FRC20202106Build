d3d5c74a671e3544bc41b90753508b6a017374b1
James DeLoach <jamesjdeloach@gmail.com>
2020-02-11 17:00:46 -0500

    Added more constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants.  This class should not be used for any other purpose.  All constants should be
@@ -15,11 +19,42 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants 
-{
-    public static final int RIGHT_JOYSTICK_ID = 0;
-    public static final int LEFT_JOYSTICK_ID = 1;
-    public static final int CONTROLLER_ID = 2;
+public final class Constants {
 
+    public static final class DriveConstants
+    {
+        public static final int kLeftMotor1Port = 0;
+        public static final int kLeftMotor2Port = 1;
+        public static final int kRightMotor1Port = 2;
+        public static final int kRightMotor2Port = 3;
 
+        
+        public static final boolean kLeftEncoderReversed = false;
+        public static final boolean kRightEncoderReversed = true;
+
+        public static final double kTrackwidthMeters = 0.6096;
+        public static final DifferentialDriveKinematics kDriveKinematics =
+            new DifferentialDriveKinematics(kTrackwidthMeters);
+
+        public static final double kEncoderCPR = 42 * 11.21;
+        public static final double kWheelDiameterMeters = 0.1524;
+        public static final double kEncoderDistancePerPulse =
+            (kWheelDiameterMeters * Math.PI) / (double) kEncoderCPR;
+
+        public static final boolean kGyroReversed = true;
+
+        public static final double ksVolts = 0.22;
+        public static final double kvVoltSecondsPerMeter = 1.98;
+        public static final double kaVoltSecondsSquaredPerMeter = 0.2;
+
+        public static final double kPDriveVel = 8.5;
+
+        
+        public static final DifferentialDriveVoltageConstraint autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
+            new SimpleMotorFeedforward(ksVolts, 
+            kvVoltSecondsPerMeter, 
+            kaVoltSecondsSquaredPerMeter), 
+            kDriveKinematics, 
+            10);
+    }
 }
