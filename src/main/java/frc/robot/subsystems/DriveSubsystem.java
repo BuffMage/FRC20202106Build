90e423f29f73f4c6ba87d3be7b36d94810ec969a
James DeLoach <jamesjdeloach@gmail.com>
2020-02-11 18:53:22 -0500

    Updated Drive

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,15 +1,31 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
 
 public class DriveSubsystem extends SubsystemBase {
 
   private static DriveSubsystem m_robotDrive = null;
   private static SpeedControllerGroup m_leftMotors;
   private static SpeedControllerGroup m_rightMotors;
+  private static CANSparkMax m_leftMotor1;
+  private static CANSparkMax m_leftMotor2;
+  private static CANSparkMax m_rightMotor1;
+  private static CANSparkMax m_rightMotor2;
+  private static DifferentialDrive m_drive;
+  private static Gyro m_gyro;
+  private static CANEncoder m_leftEncoder;
+  private static CANEncoder m_rightEncoder;
   
 
   public static DriveSubsystem getInstance()
@@ -22,9 +38,19 @@ public class DriveSubsystem extends SubsystemBase {
   }
 
   private DriveSubsystem() {
+    m_leftMotor1 = new CANSparkMax(DriveConstants.kLeftMotor1Port, MotorType.kBrushless);
+    m_leftMotor2 = new CANSparkMax(DriveConstants.kLeftMotor2Port, MotorType.kBrushless);
+    m_rightMotor1 = new CANSparkMax(DriveConstants.kRightMotor1Port, MotorType.kBrushless);
+    m_rightMotor2 = new CANSparkMax(DriveConstants.kRightMotor2Port, MotorType.kBrushless);
 
-    //m_leftMotors = new SpeedControllerGroup(speedController, speedControllers)
+    m_leftMotors = new SpeedControllerGroup(m_leftMotor1, m_leftMotor2);
+    m_rightMotors = new SpeedControllerGroup(m_rightMotor1, m_rightMotor2);
+    m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
+    m_gyro = new ADXRS450_Gyro();
 
+    m_leftEncoder = m_leftMotor1.getEncoder();
+    m_rightEncoder = m_rightMotor1.getEncoder();
+    
   }
 
   @Override
@@ -44,6 +70,8 @@ public class DriveSubsystem extends SubsystemBase {
 
   public void tankDriveVolts(double leftVolts, double rightVolts)
   {
-    
+    m_leftMotors.setVoltage(leftVolts);
+    m_rightMotors.setVoltage(-rightVolts);
+    m_drive.feed();
   }
 }
